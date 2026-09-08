1f69053de1fd414e55b59d31029b79cbfdcf96e2
Михаил <root@rhcsa-node1.example.com>
2026-09-08 16:56:29 +0300

    Modify cloud

--- outputs.tf
+++ outputs.tf
@@ -33,3 +33,8 @@ output "service_account_static_access_key" {
   value       = var.key_file 
   sensitive   = true
 }
+
+output "instance_public_ip_address" {
+  description = "The external IP address of the instance."
+  value       = yandex_compute_instance.this.network_interface.0.nat_ip_address
+}
