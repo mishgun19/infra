ff66dfb7b72803bdd75d7370c9593b76b15335ec
Михаил <root@rhcsa-node1.example.com>
2026-09-21 15:02:30 +0300

    Add new disk, security groups and cluster.

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 locals {
-  boot_disk_name      = "${var.name_prefix}-boot-disk"
-  linux_vm_name       = "${var.name_prefix}-linux-vm"
-  vpc_network_name    = "${var.name_prefix}-private"
-  ydb_serverless_name = "${var.name_prefix}-ydb-serverless"
-  bucket_sa_name      = "${var.name_prefix}-bucker-sa"
-  bucket_name         = "${var.name_prefix}-terraform-bucket-${random_string.bucket_name.result}"
+  boot_disk_name      = var.boot_disk_name != null ? var.boot_disk_name : "${var.name_prefix}-boot-disk"
+  linux_vm_name       = var.linux_vm_name != null ? var.linux_vm_name : "${var.name_prefix}-linux-vm"
+  vpc_network_name    = var.vpc_network_name != null ? var.vpc_network_name : "${var.name_prefix}-private"
+  ydb_serverless_name = var.ydb_serverless_name != null ? var.ydb_serverless_name : "${var.name_prefix}-ydb-serverless"
+  bucket_sa_name      = var.bucket_sa_name != null ? var.bucket_sa_name : "${var.name_prefix}-bucker-sa"
+  bucket_name         = var.bucket_name != null ? var.bucket_name : "${var.name_prefix}-terraform-bucket-${random_string.bucket_name.result}"
 }
 
 resource "yandex_vpc_network" "this" {
@@ -12,22 +12,59 @@ resource "yandex_vpc_network" "this" {
 }
 
 resource "yandex_vpc_address" "this" {
-  name = "${local.linux_vm_name}-address"
+  for_each = var.zones
+
+  name = length(var.zones) > 1 ? "${local.linux_vm_name}-address-${substr(each.value, -1, 0)}" : "${local.linux_vm_name}-address"
   external_ipv4_address {
-    zone_id = var.zone
+    zone_id = each.value
   }
 }
 
 resource "yandex_vpc_subnet" "private" {
-  name = keys(var.subnets)[0]
-  zone = var.zone
-  v4_cidr_blocks = var.subnets[keys(var.subnets)[0]]
+  for_each = var.zones
+
+  zone = each.value
+  name = keys(var.subnets)[index(tolist(var.zones), each.value)]
+  v4_cidr_blocks = var.subnets[each.value]
   network_id = yandex_vpc_network.this.id
 }
 
+resource "yandex_compute_disk" "secondary_disk_a" {
+  count = contains(var.zones, "ru-central1-a") ? var.secondary_disks.count : 0
+
+  name = "${var.secondary_disks.name}-a-${count.index}"
+  zone = "ru-central1-a"
+
+  type = var.secondary_disks.type
+  size = var.secondary_disks.size
+}
+
+resource "yandex_compute_disk" "secondary_disk_b" {
+  count = contains(var.zones, "ru-central1-b") ? var.secondary_disks.count : 0
+
+  name = "${var.secondary_disks.name}-b-${count.index}"
+  zone = "ru-central1-b"
+
+  type = var.secondary_disks.type
+  size = var.secondary_disks.size
+}
+
+resource "yandex_compute_disk" "secondary_disk_d" {
+  count = contains(var.zones, "ru-central1-d") ? var.secondary_disks.count : 0
+
+  name = "${var.secondary_disks.name}-d-${count.index}"
+  zone = "ru-central1-d"
+
+  type = var.secondary_disks.type
+  size = var.secondary_disks.size
+}
+
 resource "yandex_compute_disk" "boot_disk" {
-  name     = local.boot_disk_name
-  zone     = var.zone
+  name     = length(var.zones) > 1 ? "${local.boot_disk_name}-${substr(each.value, -1, 0)}" : local.boot_disk_name
+
+  for_each = var.zones
+  zone = each.value
+
   image_id = var.image_id
 
   type     = var.instance_resources.disk.disk_type
@@ -35,10 +72,12 @@ resource "yandex_compute_disk" "boot_disk" {
 }
 
 resource "yandex_compute_instance" "this" {
-  name                      = local.linux_vm_name
+  
+  for_each                  = var.zones
+  name                      = length(var.zones) > 1 ? "${local.linux_vm_name}-${substr(each.value, -1, 0)}" : local.linux_vm_name
   allow_stopping_for_update = true
   platform_id               = var.instance_resources.platform_id
-  zone                      = var.zone
+  zone                      = each.value
 
   resources {
     cores  = var.instance_resources.cores
@@ -46,13 +85,20 @@ resource "yandex_compute_instance" "this" {
   }
 
   boot_disk {
-    disk_id = yandex_compute_disk.boot_disk.id
+    disk_id = yandex_compute_disk.boot_disk[each.value].id
+  }
+
+  dynamic "secondary_disk" {
+    for_each = each.value == "ru-central1-a" ? yandex_compute_disk.secondary_disk_a : each.value == "ru-central1-b" ? yandex_compute_disk.secondary_disk_b : each.value == "ru-central1-d" ? yandex_compute_disk.secondary_disk_d : []
+    content {
+      disk_id = try(secondary_disk.value.id, null)
+    }
   }
 
   network_interface {
-    subnet_id      = yandex_vpc_subnet.private.id
+    subnet_id      = yandex_vpc_subnet.private[each.value].id
     nat            = true
-    nat_ip_address = yandex_vpc_address.this.external_ipv4_address[0].address
+    nat_ip_address = yandex_vpc_address.this[each.value].external_ipv4_address[0].address
   }
 
   metadata = {
@@ -95,4 +141,63 @@ resource "random_string" "bucket_name" {
   length  = 8
   special = false
   upper   = false
+}
+
+resource "yandex_mdb_mysql_cluster" "this" {
+  for_each = var.zones
+
+  network_id = yandex_vpc_network.this.id
+  name = "mysql-cluster"
+  environment = "PRESTABLE"
+  version = "8.0"
+
+  resources {
+    resource_preset_id = "s2.micro"
+    disk_type_id = "network-ssd"
+    disk_size = 10
+  }
+
+  host {
+    zone = each.value
+    subnet_id = yandex_vpc_subnet.private[each.value].id
+  }
+}
+
+resource "yandex_vpc_security_group" "this" {
+  network_id = yandex_vpc_network.this.id
+
+  dynamic "ingress" {
+    for_each = var.ingress_rule
+    content {
+      protocol       = ingress.value.protocol
+      description    = ingress.value.description
+      v4_cidr_blocks = ingress.value.v4_cidr_blocks
+      port           = ingress.value.port
+    }
+  }
+}
+
+resource "yandex_mdb_mysql_database" "database" {
+  for_each = length(var.databases) > 0 ? {for db in var.databases : db.name => db} : {}
+
+  cluster_id = yandex_mdb_mysql_cluster.this[each.value].id
+  name              = lookup(each.value, "name", null)
+}
+
+
+resource "yandex_mdb_mysql_user" "user" {
+  for_each = length(var.users) > 0 ? { for u in var.users : u.name => u } : {}
+  
+  cluster_id = yandex_mdb_mysql_cluster.this[each.value].id
+  name       = each.value.name
+  password   = each.value.password
+  dynamic "permission" {
+    for_each = lookup(each.value, "permissions", [])
+    content {
+      database_name = permission.value.database_name
+      roles = permission.value.roles
+    }
+  }
+
+  depends_on = [ yandex_mdb_mysql_database.database ]
 }
